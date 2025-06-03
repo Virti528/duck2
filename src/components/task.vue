e4be3c018a56291ab6db542e011913968900356b
Дмитрий Чернов <4eduma4@gmail.com>
2025-06-03 10:57:48 +0300

    o4ko 2

--- src/components/task.vue
+++ src/components/task.vue
@@ -11,16 +11,16 @@ const clickHandler = () => {
   const url = new URL("task", "https://da11os.pythonanywhere.com");
   fetch(url, {
     method: "POST",
-    body: JSON.stringify{task},
+    body: JSON.stringify(task),
     headers: [
-        ["Accept", "application/json"]
-        ["content-type", "application/json"]
-    ]
+      ["Accept", "application/json"],
+      ["content-type", "application/json"],
+    ],
   });
 };
 </script>
 <template>
   <el-input placeholder="Name" v-model="task.title"></el-input>
   <el-checkbox label="Done" v-model="task.status"></el-checkbox>
-  <el-button type="succes" @click="">Success</el-button>
+  <el-button type="succes" @click="clickHandler">Success</el-button>
 </template>
